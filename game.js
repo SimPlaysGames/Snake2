5ce0dfe450d58e4a588f0be8d0de5fbdf90781de
OscarWoHA <ohalland@gmail.com>
2018-06-01 13:06:41 +0200

    Spillet fungerer

--- game.js
+++ game.js
@@ -3,6 +3,10 @@ let width = 500,
     tickRate = 200; //TODO: Minske når man får flere poeng
 
 let snake = new Snake(4, 25);
+let apple = {
+    x: Math.floor(Math.random() * (width / 25)),
+    y: Math.floor(Math.random() * (height / 25))
+};
 
 function setup() {
     createCanvas(width, height);
@@ -13,9 +17,29 @@ function setup() {
 function draw() {
     background(0);
 
-    snake.draw();
+    if(!snake.dead) {
+        fill(255, 0, 0);
+        ellipseMode(CORNER);
+        ellipse(apple.x * 25, apple.y * 25, 25, 25);
+
+        snake.draw();
+    }
 }
 
 function tick() {
     snake.update();
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        snake.direction = 'W';
+    } else if (keyCode === RIGHT_ARROW) {
+        snake.direction = 'E';
+    }  else if (keyCode === UP_ARROW) {
+        snake.direction = 'N';
+    } else if (keyCode === DOWN_ARROW) {
+        snake.direction = 'S';
+    } else if (keyCode === 82) {
+        snake.clear();
+    }
 }
--- snake.js
+++ snake.js
@@ -7,6 +7,8 @@ class Snake {
     }
 
     draw() {
+        this.collision();
+
         for(let part of this.parts) {
             ellipseMode(CORNER);
             fill(0, 255, 0);
@@ -15,38 +17,63 @@ class Snake {
     }
 
     update() {
-        let pop = this.parts.pop();
+        let pop = this.wait ? { x: 0, y: 0} : this.parts.pop();
+
+        if(this.wait) {
+            this.wait = false;
+        }
 
         switch(this.direction) {
             case 'N':
-                pop.x = this.parts[0].x;
-                pop.y = this.parts[0].y - 1;
-                break;
+            pop.x = this.parts[0].x;
+            pop.y = this.parts[0].y - 1;
+            break;
             case 'S':
-                pop.x = this.parts[0].x;
-                pop.y = this.parts[0].y + 1;
-                break;
+            pop.x = this.parts[0].x;
+            pop.y = this.parts[0].y + 1;
+            break;
             case 'E':
-                pop.x = this.parts[0].x + 1;
-                pop.y = this.parts[0].y;
-                break;
+            pop.x = this.parts[0].x + 1;
+            pop.y = this.parts[0].y;
+            break;
             case 'W':
-                pop.x = this.parts[0].x - 1;
-                pop.y = this.parts[0].y;
-                break;
+            pop.x = this.parts[0].x - 1;
+            pop.y = this.parts[0].y;
+            break;
         }
 
         this.parts.unshift(pop);
     }
 
     collision() {
+        for(let i = 0; i < this.parts.length; i++) {
+            if(this.parts[i].x == apple.x && this.parts[i].y == apple.y) {
+                this.points++;
+                this.wait = true;
 
+                apple = {
+                    x: Math.floor(Math.random() * (width / 25)),
+                    y: Math.floor(Math.random() * (height / 25))
+                };
+            }
+
+            for(let i2 = 0; i2 < this.parts.length; i2++) {
+                if(this.parts[i] == this.parts[i2]) {
+                    continue;
+                }
+
+                if(this.parts[i].x == this.parts[i2].x && this.parts[i].y == this.parts[i2].y) {
+                    this.dead = true;
+                }
+            }
+        }
     }
 
     clear() {
         this.parts = [];
         this.points = 0;
         this.direction = 'E';
+        this.dead = false;
 
         for(let i = this.startLength - 1; i >= 0; i--) {
             this.parts.push({ x: i, y: 0 });
